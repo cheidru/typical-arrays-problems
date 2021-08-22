9ffb6b05ddebabe3bbea013980d9f19e48dd1d44
My Name <my@test.com>
2021-08-22 21:06:11 +0300

    init: create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    let result = 0;
+    for (let item of array) {
+        result = result > item ? item : result;
+    }
+
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = 0;
+    for (let item of array) {
+        result = result < item ? item : result;
+    }
+
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result = 0;
+    for (let item of array) {
+        result += item;
+    }
+
+  return result / array.length;
 }
